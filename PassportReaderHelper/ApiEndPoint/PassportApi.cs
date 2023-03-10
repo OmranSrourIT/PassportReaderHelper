81dff8838a04e84224693a06e695552652334eae
omran.srour@polaris-tek.com <omran.srour@polaris-tek.com>
2023-03-10 22:32:37 +0300

    Add project files.

---
+++ PassportReaderHelper/ApiEndPoint/PassportApi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PassportReaderHelper.ApiEndPoint
+{
+   public class PassportApiController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage RestartWindowsService()
+        {
+
+            string serviceName = "PassportReader_Service";
+
+            ServiceController serviceController = new ServiceController(serviceName);
+            try
+            {
+                if ((serviceController.Status.Equals(ServiceControllerStatus.Running)) || (serviceController.Status.Equals(ServiceControllerStatus.StartPending)))
+                {
+                    serviceController.Stop();
+                }
+                serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
+                serviceController.Start();
+                serviceController.WaitForStatus(ServiceControllerStatus.Running);
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Serivce Error" + ex.Message, Configuration.Formatters.JsonFormatter);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Serivce Is Restart", Configuration.Formatters.JsonFormatter);
+
+        }
+
+
+
+    }
+}
---
+++ PassportReaderHelper/Service1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace PassportReaderHelper
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+
+                string ServiceURLPath = ConfigurationManager.AppSettings["UrlSelfHosting"];
+
+                if (ServiceURLPath != "" || ServiceURLPath != null)
+                {
+                    var config = new HttpSelfHostConfiguration(ServiceURLPath);
+                    config.EnableCors();
+                    config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+                    config.MaxReceivedMessageSize = 2147483647; // use config for this value
+
+
+                    config.Routes.MapHttpRoute(
+                        name: "API",
+                        routeTemplate: "api/{controller}/{action}/{id}",
+                        defaults: new { controller = "AFISHome", id = RouteParameter.Optional }
+                        );
+                  
+                    HttpSelfHostServer server = new HttpSelfHostServer(config);
+                    server.OpenAsync().Wait();
+
+                    //WriteToFile("api http://localhost:12334 in done to calling" + DateTime.Now);
+
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                var stackTrace = new StackTrace(ex, true);
+                var frame = stackTrace.GetFrame(0);
+                var line = frame.GetFileLineNumber();
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
